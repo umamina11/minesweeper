9fe84eacfc9e0e6ed7f89d4d05214810a22f4eb1
umamina11 <62588358+umamina11@users.noreply.github.com>
2020-06-23 21:48:50 +0530

    basic program on constructors
    in this there are two codes one of basic parametrized constructor and  another one of default constructor

---
+++ trials/constructor.cpp
@@ -0,0 +1,50 @@
+//constructors
+//parametrized constructor
+/*#include<iostream>
+using namespace std;
+class cube
+{
+	public:
+		int s;
+		cube(int x)
+		{
+			s=x;
+		}
+};
+int main()
+{
+	cube c1(39);
+	
+	cout<<"the side of a cube "<<c1.s;
+	
+	return 0;
+	
+}*/
+
+
+
+
+//default constructor
+
+/*#include<iostream>
+using namespace std;
+class cube
+{
+	public:
+		int s;
+		cube()
+		{
+			s=13;
+			
+		}
+};
+int main()
+{
+	cube c;
+	cout<<"the side of cube is"<<c.s;
+	
+	return 0;
+}*/
+
+
+
